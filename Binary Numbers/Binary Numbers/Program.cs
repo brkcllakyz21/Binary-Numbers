d2db984e8bfa5c52ad7af42ad65dac75ea208330
brkcllakyz21 <37548000+brkcllakyz21@users.noreply.github.com>
2018-10-19 21:52:10 +0300

    Add files via upload

---
+++ Binary Numbers/Binary Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int k = 0;
+            string sayı = "";
+            Console.WriteLine("Bir Sayı Giriniz: ");
+            k = Convert.ToInt32(Console.ReadLine());
+
+            while (k > 0)
+            {
+                sayı = (Convert.ToString(k % 2)) + sayı;
+
+                k = k / 2;
+
+            }
+
+            Console.WriteLine(sayı);
+
+
+
+        }
+    }
+}
